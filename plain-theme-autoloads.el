68525c41c02157feed2c5f3464d6e018339e2837
samuel henrique dos santos <samuel@batCave.batCave.com.br>
2024-04-23 23:34:32 -0300

    fisrt commit

---
+++ plain-theme-autoloads.el
@@ -0,0 +1,28 @@
+;;; plain-theme-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "plain-theme" "plain-theme-white.el" "plain-theme-dark.el" (0 0 0 0))
+;;; Generated autoloads from plain-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(register-definition-prefixes "plain-theme" '("plain"))
+
+;;;***
+
+;;;### (autoloads nil nil ("plain-theme-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; plain-theme-autoloads.el ends here
